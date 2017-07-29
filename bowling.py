6bc6db10a1bf9ee466a8af390993faed51e6b80b
anikoBarat <aniko.barat@gmail.com>
2017-07-29 19:10:02 +0200

    Made get_value function simplier

--- bowling.py
+++ bowling.py
@@ -1,3 +1,4 @@
+import string 
 def score(game):
     result = 0
     frame = 1
@@ -31,10 +32,8 @@ def score(game):
     return result
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
+    if char in string.digits:
+        return int(char)   
     elif char == 'X' or char == 'x':
         return 10
     elif char == '/':
@@ -43,3 +42,4 @@ def get_value(char):
         return 0
     else:
         raise ValueError()
+
