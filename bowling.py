4d5737d778fefadeec8757cbf6b830eab13c16d0
anikoBarat <aniko.barat@gmail.com>
2017-07-29 20:28:57 +0200

    in_first_half conditionals are readable and more  logical, is game[i] 'X' or 'x' in one check with lower() function

--- bowling.py
+++ bowling.py
@@ -1,4 +1,6 @@
-import string 
+import string
+
+
 def score(game):
     result = 0
     frame = 1
@@ -7,33 +9,28 @@ def score(game):
         if game[i] == '/':
             result += 10 - last
         else:
-            result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
+            result += get_value(game[i])   
+        if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                result += get_value(game[i + 1])
+            elif game[i].lower() == 'x':
+                result += get_value(game[i + 1])
+                if game[i + 2] == '/':
+                    result += 10 - get_value(game[i + 1])
                 else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
-        if not in_first_half:
+                    result += get_value(game[i + 2])    
+        last = get_value(game[i])   
+        if not in_first_half or game[i].lower() == 'x':
+            in_first_half = True
             frame += 1
-        if in_first_half == True:
-            in_first_half = False
         else:
-            in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
-            frame += 1
+            in_first_half = False
     return result
+
 
 def get_value(char):
     if char in string.digits:
-        return int(char)   
+        return int(char)
     elif char == 'X' or char == 'x':
         return 10
     elif char == '/':
@@ -42,4 +39,3 @@ def get_value(char):
         return 0
     else:
         raise ValueError()
-
