c9d28958c4dfa93cd44b9e2b5fd14d05045f1c82
anikoBarat <aniko.barat@gmail.com>
2017-07-30 18:54:05 +0200

    Shorter, readable, more logical conditional in score func

--- bowling.py
+++ bowling.py
@@ -10,17 +10,14 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
-
         if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
+            if game[i] == '/' or game[i].lower() == 'x':
                 result += get_value(game[i + 1])
-            elif game[i].lower() == 'x':
-                result += get_value(game[i + 1])
+            if game[i].lower() == 'x':
                 if game[i + 2] == '/':
                     result += 10 - get_value(game[i + 1])
                 else:
                     result += get_value(game[i + 2])
-
         last = get_value(game[i])
         if not in_first_half or game[i].lower() == 'x':
             in_first_half = True
